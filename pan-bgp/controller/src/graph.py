1f970ca753e40fa5d7cf91a18b6e4dcea6469c33
silviodantonio <64018191+silviodantonio@users.noreply.github.com>
2026-04-02 20:53:04 +0200

    Cleanup graph module

--- pan-bgp/controller/src/graph.py
+++ pan-bgp/controller/src/graph.py
@@ -1,104 +1,55 @@
 from collections import deque
-import sys
-from itertools import permutations
 
 import utils
 
 # Get a logger instance
 logger = utils.get_logger(__name__)
 
-class Node:
 
-    def __init__(self):
-        self.id = None
-        # store neighbors
-        self.neighbors = set()
-
-class AS(Node):
-
-    def __init__(self):
-        super().__init__()
-        self.controlled = False
-        self.trusted = False
-        self.prefixes = set()
+def is_owner(as_number, prefix) -> bool:
+    # TODO: implement
+    return True
 
 
-class Graph:
+class AS():
 
-    def __init__(self):
-        # id: object
-        self.nodes = {}
+    def __init__(self, as_number, peers: list[int]):
+        self.number: int = as_number
+        self.controlled = True
+        self.trusted = True
+        self.prefixes: set[str] = set()
+        self.as_paths: list[int] = {}
 
-    def add_node(self, node: Node):
-        if node.id in self.nodes:
-            raise ValueError('Node with same id already exists')
+        self.peers: set[int] = set()
+        for peer in peers:
+            self.peers.add(peer)
+
+    def announces_prefix(self, prefix: str):
+        if is_owner(self.number, prefix):
+            self.prefixes.add(prefix)
         else:
-            self.nodes[node.id] = node
+            self.trusted = False
 
-    def del_node(self, node_idtorm):
 
-        node_torm = self.nodes.get(node_idtorm)
-        if node_torm is None:
-            raise ValueError('No such node')
-
-        # remove it as neighbor from all other nodes
-        for node_id, node in self.nodes.items():
-            if node_torm in node.neighbors:
-                node.neighbors.remove(node_torm)
-
-        del self.nodes[node_idtorm]
-
-    def add_edge(self, start_node_id, end_node_id):
-        start_node = self.nodes.get(start_node_id)
-        if start_node is None:
-            raise ValueError(f"No node with id {start_node_id}")
-
-        end_node = self.nodes.get(end_node_id)
-        if start_node is None:
-            raise ValueError(f"No node with id {start_node_id}")
-
-        # Undirected graph
-        start_node.neighbors.add(end_node)
-        end_node.neighbors.add(start_node)
-
-    def del_edge(self, start_node_id, end_node_id):
-        start_node = self.nodes.get(start_node_id)
-        if start_node is None:
-            raise ValueError(f"No node with id {start_node_id}")
-
-        end_node = self.nodes.get(end_node_id)
-        if start_node is None:
-            raise ValueError(f"No node with id {start_node_id}")
-
-        # Undirected graph
-        start_node.neighbors.remove(end_node)
-        end_node.neighbors.remove(start_node)
-
-    def __str__(self):
-        return_str = ''
-        for node_id, node in self.nodes.items():
-            neighbor_nodes_list = []
-            for neighbor in node.neighbors:
-                neighbor_nodes_list.append(neighbor.id)
-            return_str += f"{node_id}: {neighbor_nodes_list}\n"
-        return return_str
-
-class ASGraph(Graph):
+class NetworkGraph():
 
     def __init__(self):
-        super().__init__()
-        self.prefix_as_table = {}
+        self.ases: dict[str, AS] = {}
+        self.prefix_table: dict[str, AS] = {}
 
-    def add_node(self, node: AS):
-        super().add_node(node)
-        for prefix in node.prefixes:
-            self.prefix_as_table[prefix] = node.id
-        logger.debug("New AS node in graph")
+    def add_as(self, new_as: AS):
+        if new_as.number in self.ases:
+            raise ValueError(f"AS{new_as.number} is already in NetworkGraph")
+        else:
+            self.ases[new_as.number] = new_as
+            for prefix in new_as.prefixes:
+                self.prefix_table[prefix] = new_as
+            logger.debug("New AS node in graph")
 
     def find_all_paths(self, start_as: AS, dest_as: AS, path=[]) -> list[list[AS]]:
-    # Thanks to Gemini.
-    # WARN: not fully understood and
-    # being recursive might also work extremely poorly on large graphs.
+        # Thanks to Gemini.
+        # WARN: not fully understood and
+        # being recursive might also work extremely poorly on large graphs.
 
         current_path = path.copy()
         current_path.extend([start_as])
@@ -109,17 +60,19 @@ class ASGraph(Graph):
         paths = []
         for neighbor_as in start_as.neighbors:
             if neighbor_as not in current_path:
-                new_paths = self.find_all_paths(neighbor_as, dest_as, current_path)
+                new_paths = self.find_all_paths(
+                    neighbor_as, dest_as, current_path)
                 paths.extend(new_paths)
 
         return paths
 
     def _find_trusted_paths(self, start_as: AS, dest_as: AS, path=[]) -> list[list[AS]]:
-    # Thanks to Gemini part II (this code is copied from find_all_paths)
-    # I don't fully understand what's happening here and what is being passed during
-    # the recursion.
+        # Thanks to Gemini part II (this code is copied from find_all_paths)
+        # I don't fully understand what's happening here and what is being passed during
+        # the recursion.
 
-        as_trusted_peers = [as_peer for as_peer in start_as.neighbors if as_peer.trusted]
+        controlled_as_peers = [self.ases(as_peer_id) for as_peer_id in start_as.peers]
+        as_trusted_peers = [as_peer for as_peer in controlled_as_peers if as_peer.trusted]
 
         current_path = path.copy()
         current_path.extend([start_as])
@@ -130,17 +83,23 @@ class ASGraph(Graph):
         paths = []
         for as_trusted_peer in as_trusted_peers:
             if as_trusted_peer not in current_path:
-                new_paths = self._find_trusted_paths(as_trusted_peer, dest_as, current_path)
+                new_paths = self._find_trusted_paths(
+                    as_trusted_peer, dest_as, current_path)
                 paths.extend(new_paths)
 
         return paths
 
-    def find_trusted_paths(self, start_as: AS, dest_as: AS) -> list[list[AS]]:
-        return self._find_trusted_paths(start_as, dest_as, [])
+    def trusted_paths(self, start_as: AS, dest_as: AS) -> list[list[AS]]:
 
-    def reachable_nodes_from(self, start_as: AS) -> set[AS]:
+        start_as_component = self.bfs(start_as)
+        if dest_as in start_as_component:
+            return self._find_trusted_paths(start_as, dest_as, [])
+        else:
+            return [[]]
+
+    def bfs(self, start_as: AS) -> set[AS]:
         # use bfs to explore the graph component to which start_as belongs
-        logger.debug("Computing reachable nodes")
+        logger.debug(f"Computing reachable nodes from AS{start_as.number}")
 
         visited = set()
         nodes_deque = deque([start_as])
@@ -155,110 +114,11 @@ class ASGraph(Graph):
                 for neighbor in current_node.neighbors:
                     nodes_deque.append(neighbor)
 
-        logger.debug("Done computing reachable nodes")
-
         return visited
 
-    def get_components(self) -> list[set[AS]]:
-        logger.debug("Computing graph components")
-        # Get all components of the topology graph
-
-        graph_nodes = set()
-        for as_number, as_obj in self.nodes.items():
-            graph_nodes.add(as_obj)
-
-        components = []
-
-        # while some node is left
-        while len(graph_nodes) != 0:
-            # choose randomly a node
-            starting_node = graph_nodes.pop()
-            graph_nodes.add(starting_node)
-
-            # explore the connected component
-            logging.debug("Exploring a new component")
-            connected_component = self.reachable_nodes_from(starting_node)
-            components.append(connected_component)
-            # remove the nodes of the component from the nodes left to visit
-            graph_nodes = graph_nodes.difference(connected_component)
-
-        logger.debug("Done computing graph components")
-
-        return components
-
-    # Probably this function is not really needed. The approach of connecting
-    # components is wrong
-    def connect_components(self, l_set: set[AS], r_set: set[AS]):
-
-        logger.debug("Connecting components")
-
-        uncontrolled_l_nodes = [node for node in l_set if not node.controlled]
-        uncontrolled_r_nodes = [node for node in r_set if not node.controlled]
-
-        for l_node in uncontrolled_l_nodes:
-            for r_node in uncontrolled_r_nodes:
-                self.add_edge(l_node.id, r_node.id)
-
-        logger.debug("Done connecting components")
-
-
-    def fully_connect(self, nodes):
-        # Given a set of notes, adds edges between them in order connect them
-        # in a full mesh (clique)
-
-        # In principle, since this is an undirected graph i need to do
-        # half loop for building a full mesh. For now won't optimize for that
-
-        for current_node in nodes:
-            for new_neighbor in nodes:
-                if new_neighbor is not current_node:
-                    self.add_edge(current_node, new_neighbor)
-
-    def trusted_midpoints_sequences(self, start_as: AS, end_as: AS, num=None):
-
-        logger.debug("Attempting to compute sequence of trusted midponts")
-
-        trusted_ases = []
-
-        for node_id, node_obj in self.nodes.items():
-            if node_obj.trusted:
-                trusted_ases.append(node_obj)
-
-        logger.debug("Got list of trusted ASes")
-
-        # Since i can assume an AS can reach any other AS, that means
-        # that i can think of them as connected by a full mesh.
-        # That means that computing a path mans computing a permutation of
-        # ASes of desired length
-
-        # remove source_as and end_as
-        if start_as in trusted_ases:
-            trusted_ases.remove(start_as)
-        if end_as in trusted_ases:
-            trusted_ases.remove(end_as)
-        logger.debug("Removed start AS and end AS from trusted list")
-
-        if len(trusted_ases) == 0:
-            return []
-
-        if num is not None:
-            # check that i have enough trusted AS to compute midpoints
-            if len(trusted_ases) < num:
-                return []
-        else:
-            # num was none, so use all trusted ASes
-            num = len(trusted_ases)
-
-        logger.debug(f"Computing sequences of {num} trusted midponts")
-        # computing all sequences of trusted ASes
-        paths = [deque(permutation) for permutation in permutations(trusted_ases, r=num)]
-
-        logger.debug("Computed all permutations")
-
-        for path in paths:
-            path.appendleft(start_as)
-            path.append(end_as)
-
-        logger.debug("Added back start_as and end_as to permutations")
-
-        return paths
+    def __str__(self):
+        adjacency_dictionary_list = {}
+        for as_number, as_obj in self.ases.items():
+            as_peers = [as_peer for as_peer in as_obj.peers]
+            adjacency_dictionary_list[as_number] = as_peers
+        return str(adjacency_dictionary_list)
--- pan-bgp/controller/src/main.py
+++ pan-bgp/controller/src/main.py
@@ -1,5 +1,4 @@
 from concurrent import futures
-import copy
 
 import grpc
 
@@ -14,44 +13,41 @@ import controller_pb2_grpc  # Contains stubs and other stuf for building the ser
 # Get a pre-configured logger instance
 logger = utils.get_logger(__name__)
 
-topology_graph = graph.ASGraph()
+topology_graph = graph.NetworkGraph()
 
 # Maybe return ids instead of AS objects?
-def request_path(topology_graph: graph.ASGraph, source_as: graph.AS, dest_prefix: str, policy: str, num: int) -> list[list[graph.AS]]:
 
-        found_paths = []
+def request_path(topology_graph: graph.NetworkGraph, 
+                 source_as: graph.NetworkGraph,
+                 dest_prefix: str,
+                 policy: str,
+                 num: int) -> list[list[graph.AS]]:
 
-        # Check if the destination prefix is known
-        dest_as_id = topology_graph.prefix_as_table.get(dest_prefix)
-        if dest_as_id is None:
-            logger.info(f'No known AS owns {dest_prefix}')
-            return found_paths
-        # Get dest_as object from topology graph
-        dest_as = topology_graph.nodes.get(dest_as_id)
+    found_paths = []
 
-        logger.debug(f"Current AS topology: \n{topology_graph}")
-
-        # NOTE: other two policies to add could be:
-        # controlled_paths, controlled_midpoints.
-
-        # attempt to populate the found_paths list.
-        if policy == 'trusted_paths':
-
-            source_as_component = topology_graph.reachable_nodes_from(source_as)
-            if dest_as in source_as_component:
-                logger.debug(f"AS {source_as.id} and AS {dest_as.id} belong to the same component")
-                # Then there's some hope to find some paths.
-                logger.debug(f"Finding chain of trusted ASes for {dest_prefix}")
-                found_paths = topology_graph.find_trusted_paths(source_as, dest_as)
-
-        else:
-            # return empty list
-            logger.info("Unknown policy")
-
-        # Pick the number of paths requested
-        found_paths = found_paths[:num]
-
+    # Check if the destination prefix is known
+    dest_as_id = topology_graph.prefix_table.get(dest_prefix)
+    if dest_as_id is None:
+        logger.info(f'No known AS owns {dest_prefix}')
         return found_paths
+    # Get dest_as object from topology graph
+    dest_as = topology_graph.ases.get(dest_as_id)
+
+    logger.debug(f"Current AS topology: {topology_graph}")
+
+    # NOTE: other two policies to add could be:
+    # controlled_paths, controlled_midpoints.
+
+    # attempt to populate the found_paths list.
+    if policy == 'trusted_paths':
+        found_paths = topology_graph.trusted_paths(source_as, dest_as)
+    else:
+        logger.info("Unknown policy")
+
+    # Pick the number of paths requested
+    found_paths = found_paths[:num]
+
+    return found_paths
 
 
 # Implementation of the stub contained in pb2_grpc
@@ -59,29 +55,20 @@ class ControllerMessagingService(controller_pb2_grpc.ControllerMessagingServiceS
 
     def SendASInfo(self, request, context):
 
-        local_as = request.local_as
-        remote_as_list = request.remote_as_list
+        local_as_num = request.local_as
+        peers_list = request.remote_as_list
         prefix_list = request.prefix_list
 
-        logger.info(f"Received info message from {local_as}")
-        logger.debug(f"ASN: {local_as}, neighbors: {remote_as_list}, prefixes: {prefix_list}")
+        logger.info(f"Received info message from AS{local_as_num}")
+        logger.debug(f"ASN: {local_as_num}, neighbors: {peers_list}, prefixes: {prefix_list}")
 
         # if not exists, build a new node
-        if local_as not in topology_graph.nodes:
-            logger.info(f"Creating new node for AS {local_as}")
-            new_as = graph.AS()
-            new_as.id = local_as
-            new_as.prefixes = set(prefix_list)
-            new_as.controlled = True
-            new_as.trusted = True
-            topology_graph.add_node(new_as)
-
-        # Add edges for known neighbors
-        for neighbor_as in remote_as_list:
-            if neighbor_as in topology_graph.nodes:
-                logger.debug(f"{local_as} has known neighbor {neighbor_as}")
-                logger.debug(f"Add edge {local_as}, {neighbor_as}")
-                topology_graph.add_edge(local_as, neighbor_as)
+        if local_as_num not in topology_graph.ases:
+            logger.info(f"Creating object for AS{local_as_num}")
+            new_as = graph.AS(local_as_num, peers_list)
+            for prefix in prefix_list:
+                new_as.announces_prefix(prefix)
+            topology_graph.add_as(new_as)
 
         return controller_pb2.ResponseStatus(status="OK")
 
@@ -90,34 +77,40 @@ class ControllerMessagingService(controller_pb2_grpc.ControllerMessagingServiceS
         # Extract data from request message
         dest_prefix = request.destination.dest_prefix
         local_as_id = request.destination.local_as
-        local_as = topology_graph.nodes.get(local_as_id)
+        local_as = topology_graph.ases.get(local_as_id)
         policy = request.policy.policy
         number_paths = request.number_of_paths
 
-        logger.info(f'AS {local_as.id} requested {number_paths} paths for prefix {dest_prefix} with policy {policy}')
+        logger.info(f'AS {local_as.number} requested {number_paths} paths for prefix {dest_prefix} with policy {policy}')
 
-
-        found_paths = request_path(topology_graph, local_as, dest_prefix, policy, number_paths)
+        found_paths = request_path(
+            topology_graph, local_as, dest_prefix, policy, number_paths)
 
         # Convert paths from objects to list of ids:
         found_paths_ids = []
         for path in found_paths:
-            found_paths_ids.append([as_info.id for as_info in path])
+            found_paths_ids.append([as_info.number for as_info in path])
 
         logger.debug(f"Returning paths for {dest_prefix}:\n{found_paths_ids}")
+
         # Return a list of ASPaths. Each ASPath is a list of ASN
-        return controller_pb2.Paths(paths=[controller_pb2.ASPath(as_path=path) for path in found_paths_ids])
+        paths = [controller_pb2.ASPath(as_path=path)
+                 for path in found_paths_ids]
+        return controller_pb2.Paths(paths=paths)
+
 
 def serve(port):
     # ensure that port is a string
     port = str(port)
 
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
-    controller_pb2_grpc.add_ControllerMessagingServiceServicer_to_server(ControllerMessagingService(), server)
+    controller_pb2_grpc.add_ControllerMessagingServiceServicer_to_server(
+        ControllerMessagingService(), server)
     server.add_insecure_port("[::]:" + port)
     server.start()
     logger.info(f"Server started, listening on {port}")
     server.wait_for_termination()
+
 
 if __name__ == "__main__":
     config_data = config.load_config_file("/etc/panbgp/panbgp.conf")
